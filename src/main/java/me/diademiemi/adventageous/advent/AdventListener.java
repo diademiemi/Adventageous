a11e55639b32ed3f3851f14bd812002c11f87f50
diademiemi <emilia@diademiemi.me>
2022-12-02 12:41:57 +0100

    Use offset for reminder announcement

--- src/main/java/me/diademiemi/adventageous/advent/AdventListener.java
+++ src/main/java/me/diademiemi/adventageous/advent/AdventListener.java
@@ -8,13 +8,34 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class AdventListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        LocalDate date = LocalDate.now();
+        LocalDateTime date = LocalDateTime.now();
+        if (Advent.getOffset() != null && Advent.getOffset() != "-0" && Advent.getOffset() != "+0") {
+            String offset = Advent.getOffset();
+            // Get first character
+            char firstChar = offset.charAt(0);
+            if (firstChar == '+') {
+                // Get the rest of the string
+                String rest = offset.substring(1);
+                // Get the number
+                int number = Integer.parseInt(rest);
+                // Add the number to the date as minutes
+                date = date.plusMinutes(number);
+            } else if (firstChar == '-') {
+                // Get the rest of the string
+                String rest = offset.substring(1);
+                // Get the number
+                int number = Integer.parseInt(rest);
+                // Add the number to the date as minutes
+                date = date.minusMinutes(number);
+            }
+        }
         int year = date.getYear();
         int month = date.getMonthValue();
         int day = date.getDayOfMonth();
